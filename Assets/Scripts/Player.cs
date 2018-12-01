501d93af99cf9a7da56210f8c1f267f294014065
Magicolo <straty91@hotmail.com>
2018-11-30 22:32:22 -0500

    player death

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public enum States
+	{
+		Alive,
+		Dead
+	}
+
+	public float Speed = 100;
+	public SpriteRenderer Sprite;
+	public Rigidbody2D Body;
+	public string Horizontal = "Horizontal";
+	public string Vertical = "Vertical";
+
+	States _state = States.Alive;
+	float _respawn;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.K)) Kill(1f);
+
+		switch (_state)
+		{
+			case States.Alive:
+				Body.simulated = true;
+				Sprite.color = Color.Lerp(Sprite.color, Color.white, 3f * Time.deltaTime);
+				break;
+			case States.Dead:
+				_respawn -= Time.deltaTime;
+				Body.simulated = false;
+				Sprite.color = Color.Lerp(Sprite.color, Color.black, 3f * Time.deltaTime);
+				break;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		var input = new Vector2(Input.GetAxis(Horizontal), Input.GetAxis(Vertical));
+
+		switch (_state)
+		{
+			case States.Alive:
+				Body.AddForce(input.normalized * Speed);
+				break;
+			case States.Dead:
+				if (_respawn <= 0f && input.magnitude > 0.1f) Revive();
+				break;
+		}
+
+		var velocity = Body.velocity;
+		var magnitude = velocity.magnitude;
+		if (magnitude > Speed) Body.velocity = velocity.normalized * Speed;
+	}
+
+	public void Revive()
+	{
+		_state = States.Alive;
+	}
+
+	public void Kill(float respawn)
+	{
+		_state = States.Dead;
+		_respawn = respawn;
+	}
+}
