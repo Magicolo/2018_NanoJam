ddc0c6c6a14f6ff5004314e1ec11fdf895624206
Richard <rail.rich@gmail.com>
2018-11-30 22:27:57 -0500

    ad

--- Assets/Scripts/Plateform.cs
+++ Assets/Scripts/Plateform.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plateform : MonoBehaviour {
+public class Plateform : MonoBehaviour
+{
 
 	public MeshRenderer Mesh;
-	public PolygonCollider2D Collider;
-	
-	// Use this for initialization
-	void Start () {
-		
+	public PolygonCollider2D[] Colliders { get { return GetComponentsInChildren<PolygonCollider2D>(); }}
+
+
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
 }
