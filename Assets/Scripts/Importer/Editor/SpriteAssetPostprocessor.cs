bbbba7bcef97842034c0e89029e8e5ced66cdb88
richard <rail.rich@gmail.com>
2019-01-09 13:34:59 -0500

    Default Sprite importation settings

---
+++ Assets/Scripts/Importer/Editor/SpriteAssetPostprocessor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class SpriteAssetPostprocessor : AssetPostprocessor
+{
+	void OnPreprocessTexture()
+	{
+		var importer = (TextureImporter)assetImporter;
+		Object asset = AssetDatabase.LoadAssetAtPath(assetImporter.assetPath, typeof(Texture2D));
+
+		if (!asset)
+		{
+			importer.textureType = SpriteAssetPostprocessorEditorWindow.textureType;
+			importer.isReadable = SpriteAssetPostprocessorEditorWindow.isReadable;
+			importer.filterMode = SpriteAssetPostprocessorEditorWindow.filterMode;
+			importer.compressionQuality = (int)SpriteAssetPostprocessorEditorWindow.compressionQuality;
+			importer.spritePixelsPerUnit = SpriteAssetPostprocessorEditorWindow.spritePixelsPerUnit;
+		}
+	}
+}
---
+++ Assets/Scripts/Importer/Editor/SpriteAssetPostprocessorEditorWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[System.Serializable]
+public class SpriteAssetPostprocessorEditorWindow : EditorWindow
+{
+	public static TextureImporterType textureType = TextureImporterType.Sprite;
+	public static bool isReadable = true;
+	public static FilterMode filterMode = FilterMode.Point;
+	public static CompressionLevel compressionQuality = CompressionLevel.None;
+	public static float spritePixelsPerUnit = 16;
+
+	[MenuItem("Window/Sprite AssetPostprocessor")]
+	static void Init()
+	{
+		GetInstance().Show();
+	}
+
+	public static SpriteAssetPostprocessorEditorWindow GetInstance(){
+		return EditorWindow.GetWindow<SpriteAssetPostprocessorEditorWindow>();
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label("Default Settings", EditorStyles.boldLabel);
+		EditorGUI.BeginChangeCheck();
+		textureType = (TextureImporterType)EditorGUILayout.EnumPopup("Texture Type", textureType);
+		isReadable = EditorGUILayout.Toggle("Is Readable",isReadable);
+		filterMode = (FilterMode)EditorGUILayout.EnumPopup("Filter Mode", filterMode);
+		compressionQuality = (CompressionLevel)EditorGUILayout.EnumPopup("Compression Quality", compressionQuality);
+		spritePixelsPerUnit = EditorGUILayout.FloatField("Sprite PixelsPerUnit", spritePixelsPerUnit);
+
+		EditorGUI.EndChangeCheck();
+	}
+}
